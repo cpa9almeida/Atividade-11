d00a0678a3796924e05b796de5b7e60fc8bf24b5
cpa9almeida <133358082+cpa9almeida@users.noreply.github.com>
2023-08-11 11:41:54 +0000

    i

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
 import {selectUsuarios, selectUsuario } from "./bd.js";
 
+
 dotenv.config();
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
@@ -13,19 +14,27 @@ app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   console.log("Rota / solicitada");
 });
 
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
+  try {
+    const usuario = await selectUsuario();
+    res.json(usuario);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
+
 app.listen(port, () => {            // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
-//index.js
-import dotenv from "dotenv";
 
-dotenv.config();
-import { selectUsuarios } from "./bd.js";
-app.get("/usuarios", async (req, res) => {
-  console.log("Rota GET/usuarios solicitada");
+//index.js
+app.get("/usuario/:id", async (req, res) => {
+  console.log("Rota GET /usuario solicitada");
   try {
-    const usuarios = await selectUsuarios();
-    res.json(usuarios);
+    const usuario = await selectUsuario(req.params.id);
+    if (usuario.length > 0) res.json(usuario);
+    else res.status(404).json({ message: "Usuário não encontrado!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
