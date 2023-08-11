3a304fca7c6846ad0e2e1737e7742a4302ab44a4
cpa9almeida <133358082+cpa9almeida@users.noreply.github.com>
2023-08-11 11:12:35 +0000

    i

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
 import express from "express";      // Requisição do pacote do express
+import {selectUsuarios, selectUsuario } from "./bd.js";
+
+dotenv.config();
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
@@ -22,20 +26,6 @@ app.get("/usuarios", async (req, res) => {
   try {
     const usuarios = await selectUsuarios();
     res.json(usuarios);
-  } catch (error) {
-    res.status(error.status || 500).json({ message: error.message || "Erro!" });
-  }
-});
-
-//index.js
-import { selectUsuarios, selectUsuario } from "./bd.js";
-//index.js
-app.get("/usuario/:id", async (req, res) => {
-  console.log("Rota GET /usuario solicitada");
-  try {
-    const usuario = await selectUsuario(req.params.id);
-    if (usuario.length > 0) res.json(usuario);
-    else res.status(404).json({ message: "Usuário não encontrado!" });
   } catch (error) {
     res.status(error.status || 500).json({ message: error.message || "Erro!" });
   }
