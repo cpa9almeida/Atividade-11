eaa6bf2edbb223be06d80e0954806f5b981f322c
cpa9almeida <133358082+cpa9almeida@users.noreply.github.com>
2023-09-01 12:05:51 +0000

    a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import roteadorUsuario from "./routes/usuario.js";
+import roteadorLogin from "./routes/login.js";
 dotenv.config();
 
 const app = express();
@@ -8,6 +9,8 @@ const port = 3000;
 
 app.use(express.json());
 app.use(roteadorUsuario);
+app.use(roteadorLogin);
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.json({
--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -7,6 +7,7 @@ import {
   deleteUsuario,
   updateUsuario,
 } from "../db/index.js";
+import verificarAutenticacao from "../middlewares/autenticacao.js";
 
 const router = Router();
 
@@ -32,7 +33,7 @@ router.get("/usuario/:id", async (req, res) => {
   }
 });
 
-router.post("/usuario", async (req, res) => {
+router.post("/usuario",verificarAutenticacao, async (req, res) => {
   console.log("Rota POST /usuario solicitada");
   try {
     await insertUsuario(req.body);
@@ -42,7 +43,7 @@ router.post("/usuario", async (req, res) => {
   }
 });
 
-router.put("/usuario", async (req, res) => {
+router.put("/usuario", verificarAutenticacao, async (req, res) => {
   console.log("Rota PUT /usuario solicitada");
   try {
     const usuario = await selectUsuario(req.body.id);
@@ -56,7 +57,7 @@ router.put("/usuario", async (req, res) => {
   }
 });
 
-router.delete("/usuario/:id", async (req, res) => {
+router.delete("/usuario/:id", verificarAutenticacao,  async (req, res) => {
   console.log("Rota DELETE /usuario solicitada");
   try {
     await deleteUsuario(req.params.id);
